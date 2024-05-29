848574bf4937f29ea69e34cb0913819a27c5fbba
AanelyaA <yerzhanova.anelya@gmail.com>
2024-05-29 16:00:27 -0400

    added style partials and a components page, favicon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         {/* <Route path="/" element={<HomePage />} /> */}
         {/* <Route path="/login" element={<Login />} /> */}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div>
+        <img />
+        <h3>seal pill</h3>
+      </div>
+      <nav>
+        <h3>home</h3>
+        <h3>profile</h3>
+      </nav>
+    </header>
+  );
+}
