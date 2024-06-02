58cafa9db66ff31c0190da5911f8081950d6ba9c
AanelyaA <yerzhanova.anelya@gmail.com>
2024-06-02 15:23:04 -0400

    styles patient card for mobile

--- src/components/PatientUsers/PatientUsers.js
+++ src/components/PatientUsers/PatientUsers.js
@@ -3,11 +3,13 @@ import "./PatientUsers.scss";
 export default function PatientUsers() {
   return (
     <div className="user">
-      <h1>Lady Cat</h1>
-      <h3>Date of Birth</h3>
-      <h3>Primary Doctor</h3>
-      <h3>Allergies</h3>
-      <h3>Past Medications</h3>
+      <h2 className="user__title">Lady Cat</h2>
+      <div className="user__container">
+        <h3 className="user__label">Date of Birth: 17.12.2012</h3>
+        <h3 className="user__label">Primary Doctor: Dr. Meow</h3>
+        <h3 className="user__label">Allergies: NKDA</h3>
+      </div>
+      <h3 className="user__link">Past Medications</h3>
     </div>
   );
 }
