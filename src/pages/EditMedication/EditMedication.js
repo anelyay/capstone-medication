e4960d296a82b9b2a11551b05a978192adae7c1f
AanelyaA <yerzhanova.anelya@gmail.com>
2024-06-04 23:00:09 -0400

    styled and updated medication page, also updated logo

--- src/pages/EditMedication/EditMedication.js
+++ src/pages/EditMedication/EditMedication.js
@@ -2,5 +2,13 @@ import MedicationForm from "../../components/MedicationForm/MedicationForm";
 import "./EditMedication.scss";
 
 export default function EditMedication() {
-  return <MedicationForm className="edit-med" title="Edit Medication" />;
+  return (
+    <MedicationForm
+      className="edit-med"
+      title="Edit Medication"
+      buttonName="submit"
+      buttonSecond="delete"
+      // handleSecond={handleSecond}
+    />
+  );
 }
--- src/pages/MedicationPage/MedicationPage.js
+++ src/pages/MedicationPage/MedicationPage.js
@@ -1,11 +1,13 @@
 import "./MedicationPage.scss";
 import { useEffect, useState } from "react";
 import MedicationAPI from "../../classes/medicationAPI";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import otterMedication from "../../assets/images/med_otter.png";
 
 export default function MedicationPage() {
   const { id } = useParams();
   const [medication, setMedication] = useState();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getMedication = async () => {
@@ -32,37 +34,86 @@ export default function MedicationPage() {
     return 0;
   });
 
+  const navigateEdit = (event) => {
+    // navigate(`/medication/${id}/edit`);
+    navigate(`/medication/id/edit`);
+  };
   return (
     <div className="medication">
-      <div className="medication__wrap">
-        <h1 className="medication__title">Medication</h1>
-
-        <div className="medication__card">
-          <div className="medication__title-box">
-            <h2 className="medication__title">{medication.med_name}</h2>
-          </div>
-
-          <div className="medication__columns">
+      <div className="medication__card">
+        <div className="medication__title-box">
+          <h2 className="medication__title">{medication.med_name}</h2>
+        </div>
+        <div className="medication__wrap">
+          <div className="medication__container">
             <div className="medication__details">
-              <p className="medication__dose">{medication.med_dose}</p>
-              <p className="medication__quantity">
+              <h3 className="medication__heading">Details:</h3>
+              <p className="medication__text">Dose: {medication.med_dose}</p>
+              <p className="medication__text">
                 Quantity: {medication.quantity}
               </p>
-              <p className="medication__schedule">Schedule</p>
+            </div>
+
+            <div className="medication__schedule-box">
+              <h3 className="medication__heading">Schedule:</h3>
               {Array.isArray(medication.schedule) &&
               medication.schedule.length > 0 ? (
                 sortedSchedule.map((time, index) => (
-                  <p key={`${time.med_time}-${index}`}>{time.med_time}</p>
+                  <p
+                    key={`${time.med_time}-${index}`}
+                    className="medication__text"
+                  >
+                    time: {time.med_time}
+                  </p>
                 ))
               ) : (
                 <p>No schedule available</p>
               )}
             </div>
 
-            <div className="medication__more">
-              <p className="medication__more-title">{/* More Information */}</p>
-            </div>
+            {medication.notes && (
+              <div className="medication__more">
+                <h3 className="medication__heading">Notes:</h3>
+                <p className="medication__text">{medication.notes}</p>
+              </div>
+            )}
+
+            {/* {medication.activityLog && (
+            <div className="medication__activity-log">
+              <h3 className="medication__heading">Activity Log:</h3>
+              <ul className="medication__activity-list">
+                {medication.activityLog.map((log, index) => (
+                  <li key={index}>
+                    <p>Taken At: {log.taken_at}</p>
+                    <p>Quantity: {log.quantity}</p>
+                  </li>
+                ))} */}
           </div>
+
+          <div className="medication__picture">
+            <img
+              src={otterMedication}
+              alt="otter with a pill"
+              className="medication__image"
+            />
+          </div>
+        </div>
+
+        <div className="medication__buttons">
+          <button
+            type="button"
+            className="medication__button medication__button--delete"
+            // onClick={handleDelete}
+          >
+            delete
+          </button>
+          <button
+            type="button"
+            className="medication__button"
+            onClick={navigateEdit}
+          >
+            edit
+          </button>
         </div>
       </div>
     </div>
