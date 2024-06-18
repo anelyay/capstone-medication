e9c2532523f958bd44d2af2b13233a90a820e395
AanelyaA <yerzhanova.anelya@gmail.com>
2024-06-17 21:31:04 -0400

    added a reminder

---
+++ src/components/Reminder/Reminder.js
@@ -0,0 +1,10 @@
+import "../Reminder.scss"
+
+export default function Reminder ({pill, quantity}) {
+
+    return (
+        <div>
+            <p>Seems like you are going to run out of {pill} soon. You have {quantity} pills left.</p>
+        </div>
+    )
+}
