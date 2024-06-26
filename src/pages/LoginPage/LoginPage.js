dc05a81182d9cd031a965b35554df4f697e8f3ff
AanelyaA <yerzhanova.anelya@gmail.com>
2024-06-26 11:30:11 -0400

    added a timezone selection on login page

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -4,12 +4,13 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import AuthAPI from "../../classes/authAPI";
 
-export default function LoginPage({onLogin}) {
+export default function LoginPage({ onLogin }) {
   const [formData, setFormData] = useState({
     username: "",
     email: "",
     password: "",
     verifyPassword: "",
+    timezone: "",
   });
 
   const [loginData, setLoginData] = useState({
@@ -22,6 +23,38 @@ export default function LoginPage({onLogin}) {
   const [success, setSuccess] = useState(false);
 
   const navigate = useNavigate();
+
+  const timezones = [
+    { value: "Pacific/Midway", label: "(UTC-11:00) Pacific/Midway" },
+    { value: "Pacific/Honolulu", label: "(UTC-10:00) Pacific/Honolulu" },
+    { value: "America/Anchorage", label: "(UTC-09:00) Alaska Time" },
+    { value: "America/Los_Angeles", label: "(UTC-08:00) Pacific Time" },
+    { value: "America/Denver", label: "(UTC-07:00) Mountain Time" },
+    { value: "America/Chicago", label: "(UTC-06:00) Central Time" },
+    { value: "America/New_York", label: "(UTC-05:00) Eastern Time" },
+    { value: "America/Halifax", label: "(UTC-04:00) Atlantic Time" },
+    { value: "America/Caracas", label: "(UTC-04:00) Venezuela Time" },
+    { value: "America/Santiago", label: "(UTC-03:00) Chile Time" },
+    { value: "America/Buenos_Aires", label: "(UTC-03:00) Argentina Time" },
+    { value: "Atlantic/Azores", label: "(UTC-01:00) Azores Time" },
+    { value: "UTC", label: "(UTC+00:00) Coordinated Universal Time (UTC)" },
+    { value: "Europe/London", label: "(UTC+00:00) Greenwich Mean Time" },
+    { value: "Europe/Paris", label: "(UTC+01:00) Central European Time" },
+    { value: "Europe/Moscow", label: "(UTC+03:00) Moscow Time" },
+    { value: "Asia/Dubai", label: "(UTC+04:00) Gulf Standard Time" },
+    { value: "Asia/Karachi", label: "(UTC+05:00) Pakistan Standard Time" },
+    { value: "Asia/Dhaka", label: "(UTC+06:00) Bangladesh Standard Time" },
+    { value: "Asia/Jakarta", label: "(UTC+07:00) Western Indonesia Time" },
+    { value: "Asia/Shanghai", label: "(UTC+08:00) China Standard Time" },
+    { value: "Asia/Tokyo", label: "(UTC+09:00) Japan Standard Time" },
+    { value: "Australia/Sydney", label: "(UTC+10:00) Eastern Australia Time" },
+    { value: "Pacific/Noumea", label: "(UTC+11:00) New Caledonia Time" },
+    {
+      value: "Pacific/Auckland",
+      label: "(UTC+12:00) New Zealand Standard Time",
+    },
+    { value: "Pacific/Tongatapu", label: "(UTC+13:00) Tonga Time" },
+  ];
 
   const handleActive = (formName) => {
     setActiveForm(formName);
@@ -48,14 +81,14 @@ export default function LoginPage({onLogin}) {
   const handleSignSubmit = async (event) => {
     event.preventDefault();
 
-    const { username, email, password, verifyPassword } = formData;
+    const { username, email, password, verifyPassword, timezone } = formData;
 
     if (password !== verifyPassword) {
       setError("Passwords do not match");
       return;
     }
     try {
-      await AuthAPI.Register({ username, email, password });
+      await AuthAPI.Register({ username, email, password, timezone });
       setSuccess(true);
       setError(null);
       setFormData({
@@ -63,11 +96,12 @@ export default function LoginPage({onLogin}) {
         email: "",
         password: "",
         verifyPassword: "",
+        timezone:""
       });
 
       setLoginData({
         email: email,
-        password: ""
+        password: "",
       });
       setTimeout(() => setActiveForm("login"), 2000);
     } catch (error) {
@@ -187,6 +221,23 @@ export default function LoginPage({onLogin}) {
                   onChange={handleChange}
                   placeholder="Please enter your email"
                 />
+              </div>
+              <div className="login__form-box">
+                <label className="login__label">Timezone:</label>
+                <select
+                  className="login__input"
+                  name="timezone"
+                  value={formData.timezone}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Select your timezone</option>
+                  {timezones.map((tz) => (
+                    <option key={tz.value} value={tz.value}>
+                      {tz.label}
+                    </option>
+                  ))}
+                </select>
               </div>
               <div className="login__form-box">
                 <label className="login__label">Password:</label>
