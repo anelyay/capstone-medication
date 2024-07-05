242e64f4c4d8c411a6b3d2de01516293c9b38bf1
AanelyaA <yerzhanova.anelya@gmail.com>
2024-07-05 19:43:02 -0400

    removed refresh button

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -130,7 +130,7 @@ export default function ProfilePage() {
         />
       )}
 
-      <button onClick={handleRefresh}>REFRESH</button>
+      {/* <button onClick={handleRefresh}>REFRESH</button> */}
     </div>
   );
 }
